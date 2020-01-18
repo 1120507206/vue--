6659e9c7e5f196197a9fa69f0afb70ccd8597ee9
1120507206 <1120507206@qq.com>
2020-01-18 18:00:53 +0800

    第二天

--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+// 导入全局样式
 import './assets/css/global.css'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+ import axios from 'axios'
+ axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+ Vue.prototype.$http = axios
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
+import Home from '../components/home.vue'
 
 
 Vue.use(VueRouter)
@@ -12,7 +13,9 @@ const routes = [
 const router = new VueRouter({
   routes:[
 	  {path:'/',redirect:'/login'},
-	  {path:'/login',component:Login}
+	  {path:'/login',component:Login},
+	  {path:'/home',component:Home}
+	  
   ]
 })
 
